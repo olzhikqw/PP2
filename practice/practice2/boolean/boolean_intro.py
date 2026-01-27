4eaba9c6a0f0f003f0632529b7f94b37ed49525e
olzhasqw <olzhasqw>
2026-01-27 16:20:43 +0500

    commit

--- practice/practice2/boolean/boolean_intro.py
+++ practice/practice2/boolean/boolean_intro.py
@@ -1,3 +1,17 @@
 # Booleans represent one of two values: True or False.
 # In programming you often need to know if an expression is True or False.
 # You can evaluate any expression in Python, and get one of two answers, True or False.
+#1
+print(True)    # True
+
+#2
+print(False)   # False
+
+#3
+print(5 == 5)  # True
+
+#4
+print(5 > 10)  # False
+
+#5
+print(True and False)  # False
